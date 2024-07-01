0bf91f56185d7c454c4e86fa9b87b8c90e47350d
Jester <delacruz.j.bsinfotech@gmail.com>
2024-07-01 17:27:26 +0800

    Created needed files

---
+++ assets/scripts/main.js
@@ -0,0 +1,12 @@
+const gameboard = (() => {
+    const board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    
+    const getBoard = () => board;
+
+    return { getBoard, board }
+})();
+
+const updateBoard = (index, marker) => {
+
+};
+
